a6475801edc7664379007b02f9d5b0062785c2a4
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-07-20 18:04:57 -0400

    more styling

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-// import './About.css'
+import './About.css'
 
 class About extends Component {
   render(){
     return(
       <div className="about">
-        <h2>About Swapt</h2>
-        <p> Cool description of Swapt.</p>
+        <h3>What is Swapt?</h3>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper accumsan nibh at tempor. Pellentesque eu porta nibh. Fusce dictum massa at dui dignissim, sed placerat tortor tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin eget enim vitae diam aliquam commodo. In porttitor lacinia odio sit amet tempor. Mauris vitae dolor ac augue tincidunt mollis id nec sapien.</p>
+
+        <h3>Why should we Swap things?</h3>
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper accumsan nibh at tempor. Pellentesque eu porta nibh. Fusce dictum massa at dui dignissim, sed placerat tortor tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin eget enim vitae diam aliquam commodo. In porttitor lacinia odio sit amet tempor. Mauris vitae dolor ac augue tincidunt mollis id nec sapien.</p>
+
+        <h3>Who are We?</h3>
+
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse semper accumsan nibh at tempor. Pellentesque eu porta nibh. Fusce dictum massa at dui dignissim, sed placerat tortor tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin eget enim vitae diam aliquam commodo. In porttitor lacinia odio sit amet tempor. Mauris vitae dolor ac augue tincidunt mollis id nec sapien.</p>
       </div>
     )
   }
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import './Search.css'
 
 class Search extends Component {
   render(){
     return(
-      <div>
+      <div className="search">
+        <h3>Search Items</h3>
         <form onSubmit={this.props.onSubmitQuery}>
           <input type="text" placeholder="search items" onChange={this.props.handleSearchInput}></input>
           <button type="submit">Search</button>
